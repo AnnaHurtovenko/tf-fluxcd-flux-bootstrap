3b72ea23709a1151ef12669575bd071574720186
AnnaHurtovenko <163589983+AnnaHurtovenko@users.noreply.github.com>
2024-05-06 20:49:20 +0300

    main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "flux" {
   kubernetes = {
-    config_path = var.config_path
+    config_path = ${path.module}/kind-config"
   }
   git = {
     url = "https://github.com/${var.github_repository}.git"
